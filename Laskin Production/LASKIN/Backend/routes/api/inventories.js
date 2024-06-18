15a76d3913a830aefc63da3f5cf08af87a020088
rabbit-studio <info@rabbitstudio.co>
2024-06-17 19:54:47 -0500

    Remove

--- Laskin Production/LASKIN/Backend/routes/api/inventories.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router();
-const InventoryController = require('../../controllers/InventoryController');
-const auth = require('../../middleware/auth');
-
-module.exports = router;
